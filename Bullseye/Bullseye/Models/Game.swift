90efccb916211317497eb3f88cf5d78c4066f1eb
Jun Shin <69499549+greenthings@users.noreply.github.com>
2021-10-20 20:30:06 +0900

    Polish app

--- Bullseye/Bullseye/Models/Game.swift
+++ Bullseye/Bullseye/Models/Game.swift
@@ -8,11 +8,30 @@
 import Foundation
 
 struct Game {
-    var target: Int = 42
+    var target: Int = Int.random(in:1...100)
     var score: Int = 0
     var round: Int = 1
     
     func point(sldierValue: Int) -> Int {
-        return 999
+//        var difference : Int = 0
+//
+//        if sldierValue > self.target {
+//            difference = sldierValue - self.target
+//        }else if self.target < sldierValue{
+//            difference = self.target - sldierValue
+//        }else{
+//            difference = 0
+//        }
+    
+        
+        let difference: Int = abs(self.target - sldierValue)
+      
+//        if difference < 0 {
+//            difference *= -1
+//        }
+        
+        let awardedPoints: Int = 100 - difference
+        
+        return awardedPoints
     }
 }
--- Bullseye/Bullseye/Views/ContentView.swift
+++ Bullseye/Bullseye/Views/ContentView.swift
@@ -14,48 +14,91 @@ struct ContentView: View {
     @State private var game: Game = Game()
     
     var body: some View {
-              VStack {
-                Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN DO")
-                    .bold()
-                    .kerning(2.0)
-                    .multilineTextAlignment(.center)
-                    .lineSpacing(4.0)
-                    .font(.footnote)
-            Text(String(game.target))
-                    .fontWeight(.black)
-                    .kerning(-1.0)
-                    .font(.largeTitle)
-                HStack {
-                    Text("1")
-                        .bold()
-                        .fontWeight(.black)
-                    Slider(value:self.$sliderValue, in:1.0...100.0)
-                    Text("100")
-                        .bold()
-                        .fontWeight(.black)
-                }
-                Button(action: {
-                    print("It is worked")
-                    self.alertIsVisible = true
-                }) {
-                    Text("Hit me")
-                }.alert(isPresented: $alertIsVisible,
-                    content: {
-                        var roundedVaule: Int = Int(self.sliderValue.rounded())
-                        return Alert(title: Text("THE SLIDERS VALUE IS"), message: Text("You scored \(roundedVaule)." + "You scored \(self.game.point(sldierValue: roundedVaule)) points this round"), dismissButton: .default(Text("Start New Round")))
-                    
-                })
-              
+        ZStack {
+            Color("BackgroundColor")
+            .edgesIgnoringSafeArea(.all)
+            VStack {
+                InstructionView(game: $game)
+                SliderView(sliderValue: $sliderValue)
+                HitMeButton(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
             }
+        }
            
         }
     
 }
+
+struct HitMeButton: View{
+    
+    @Binding var alertIsVisible: Bool
+    @Binding var sliderValue: Double
+    @Binding var game: Game
+    
+    var body: some View{
+        Button(action: {
+            print("It is worked")
+            self.alertIsVisible = true
+        }) {
+            Text("Hit me".uppercased())
+                .bold()
+                .font(.title3)
+        }
+        .padding(20.0)
+        .background(
+            ZStack {
+                Color("ButtonColor")
+                LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.4), Color.clear]), startPoint: .top, endPoint: .bottom)
+
+            }
+        )
+        .foregroundColor(Color.white)
+        .cornerRadius(21.0)
+        .alert(isPresented: $alertIsVisible,
+            content: {
+                let roundedVaule: Int = Int(self.sliderValue.rounded())
+                return Alert(title: Text("THE SLIDERS VALUE IS"), message: Text("You scored \(roundedVaule)." + "You scored \(self.game.point(sldierValue: roundedVaule)) points this round"), dismissButton: .default(Text("Start New Round")))
+            
+        })
+        
+    }
+}
+
+struct SliderView: View{
+    @Binding var sliderValue: Double
+    var body: some View{
+        HStack {
+            SliderLabelText(text: "1")
+            Slider(value:self.$sliderValue, in:1.0...100.0)
+            SliderLabelText(text: "100")
+        }
+        .padding()
+        .foregroundColor(Color("TextColor"))
+    }
+}
+
+struct InstructionView: View{
+    @Binding var game:Game
+    var body: some View{
+        InstructionText(text: "🎯🎯🎯\nPut the bullseye as close as you can to")
+            .padding(.leading, 30.0)
+            .padding(.trailing, 30.0)
+        BigNumberText(text:String(game.target))
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
         ContentView()
             .previewLayout(.fixed(width: 568, height: 320))
+        
+        ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.dark)
+            .previewLayout(.fixed(width: 568, height: 320))
+            
     }
 }
